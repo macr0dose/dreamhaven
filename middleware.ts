60254d8de311d432d4647b8d35ec8b4094714635
macr0dose <121298889+macr0dose@users.noreply.github.com>
2024-02-26 15:50:31 +1000

    Update publicRoutes in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // Routes that can be accessed while signed out
   // Include the root path '/' here
-  publicRoutes: ['/api/webhooks/clerk'],
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
   
   // Routes that can always be accessed, and have
   // no authentication information
